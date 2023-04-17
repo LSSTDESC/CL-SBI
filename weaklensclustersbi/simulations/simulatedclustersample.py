6f4cbea48a2b9b6e00c3e7cca180a96a4d760581
Camille Avestruz <cavestru@umich.edu>
2023-04-17 13:32:28 -0400

    name clarity in simulatedclustersample

---
+++ weaklensclustersbi/simulations/simulatedclustersample.py
@@ -0,0 +1,18 @@
+"""
+Core structures and tools for dealing with a simulated sample of galaxy clusters
+
+Notes::
+
+    [sample format] weaklensingclustersbi currently supports samples defined with a given mean richness mass relation, or mean concentration-mass relation
+
+Copyright 2022-2023, LSST-DESC
+"""
+
+# Define a mass sample with some noise, and concentration sample that scatters
+# about the theoretical prediction
+
+log10mass_sample = np.random.uniform(13,15,size=10000)
+non_noisy_concentration_sample = get_concentration(log10mass_sample)
+noisy_concentration = np.random.normal(non_noisy_concentration_sample,0.2,10000)
+
+
