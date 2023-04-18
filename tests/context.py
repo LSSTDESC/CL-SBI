d9b35eec0e9771e63f64997c15964aa64ba22578
Camille Avestruz <cavestru@umich.edu>
2023-04-18 13:48:03 -0400

    added context.py and imports for tests and examples to work from their current directory to avoid needing the expectation of the package to be installed in site-packages

---
+++ tests/context.py
@@ -0,0 +1,9 @@
+''' 
+To give the individual tests import context
+'''
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import weaklensclustersbi
--- tests/test_population.py
+++ tests/test_population.py
@@ -1,12 +1,12 @@
 """
 tests for population.py
 """
-from weaklensingclustersbi.simulations import population
+from context import weaklensclustersbi as wlcs
 from numpy.testing import assert_array_less
 
 def test_generate_concentration_for_sample() :
     log10masses = np.random.uniform(13,15,size=10)
-    concentrations = population.generate_concentration_for_sample(log10masses=log10masses)
+    concentrations = wlcs.population.generate_concentration_for_sample(log10masses=log10masses)
     min_concentrations = np.ones(10) * 4
     max_concentrations = np.ones(10) * 10
     assert_array_less(min_concentrations, concentrations)
