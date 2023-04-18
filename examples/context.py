59a9a1e84f617eebd10a1217217c5ca3941c3450
Camille Avestruz <cavestru@umich.edu>
2023-04-18 13:51:56 -0400

    moved examples up one directory level

--- examples/context.py
+++ examples/context.py
@@ -4,6 +4,6 @@ To give the individual tests import context
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import weaklensclustersbi
