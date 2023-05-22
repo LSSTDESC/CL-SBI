a4a7082af426dd330c466578498396690dc744d4
Akum <akum.singh@gmail.com>
2023-05-22 18:58:42 -0400

    using sim-config
    using a config file for passing in parameters used in generating the simulations that we're going to run inference on.
    
    next steps: use a config file for inference itself. also, pass in the config json file directly so we don't need to fetch that

---
+++ examples/config_example.py
@@ -0,0 +1,43 @@
+''' 
+Experiment setup
+* One output_dir for each sim_config
+* One directory for each infer config (including the sim_output_dir that it reads from)
+
+Experiment:
+* Generate simulations using sim_config (including the output dir)
+* Run inference using infer_config, writing back to the same directory that infer_config is in
+
+
+'''
+import json
+
+sim_config = {
+    'rm_relation': 'murata17',
+    'mc_relation': 'child18',
+
+    # number of simulations
+    'num_samples': 10000,
+    'sample_noise_dex': 0,
+
+    # richness band and number of masses drawn from it
+    'min_richness': 30,
+    'max_richness': 40,
+    'num_drawn': 10,
+    # TODO: how do we want to add other noise "profiles" other than dex
+    'drawn_noise_dex': 0.2,
+    'num_radial_bins': 30,
+    'sim_output_dir': 'examples'
+}
+
+with open(f"{sim_config['sim_output_dir']}/sim_config.json", "w") as outfile:
+    json.dump(sim_config, outfile)
+
+infer_config = {
+    'sim_output_dir': 'sim_output',
+    'inference_type': 'sbi',
+    'profile_noise_dex': 0.3,
+}
+
+plot_config = {
+    'plot_type': 'cc',  # chainconsumer
+}
--- examples/simulate_wl_profile.py
+++ examples/simulate_wl_profile.py
@@ -4,16 +4,17 @@ Example script to simulate a weak lensing profile using modules
 
 from context import population, wlprofile
 import numpy as np
+import json
+from pathlib import Path
 
 # Define a richness band from which we draw masses with some noise, and the
 # concentration that scatters about that theoretical prediction
-# TODO: this should go into a config file
-num_samples = 10000
-num_drawn = 10
-min_richness = 30
-max_richness = 40
-noise_dex = 0
-rbins = 10**np.arange(0, 3, 0.1)
+
+# Open the copy of sim_config in the same directory as this script
+# TODO: have the input directory passed in as well
+p = Path(__file__).with_name('sim_config.json')
+with p.open('r') as f:
+    sim_config = json.load(f)
 '''
 * simulated_nfw_profiles: needs to be 10k from randomly sampled log10masses in 
     range 13-15 and their corresponding concentrations
@@ -22,25 +23,31 @@ rbins = 10**np.arange(0, 3, 0.1)
 
 # ~10k randomly sampled log10masses and their corresponding concentrations.
 # These are the "simulations" that we'll use for SBI
-sample_mc_pairs = population.random_mass_conc(13, 15, num_samples)
+sample_mc_pairs = population.random_mass_conc(13, 15,
+                                              sim_config['num_samples'])
+rbins = 10**np.arange(0, sim_config['num_radial_bins'] / 10, 0.1)
 simulated_nfw_profiles = np.array([
     wlprofile.simulate_nfw(log10mass, concentration, rbins)
     for log10mass, concentration in sample_mc_pairs
 ])
 
 # These are the ~10 log10masses that we've drawn from our richness bin of interest
-drawn_mc_pairs = population.gen_mc_pairs_in_richness_bin(min_richness,
-                                                         max_richness,
-                                                         num_drawn=num_drawn,
-                                                         noise_dex=noise_dex)
+drawn_mc_pairs = population.gen_mc_pairs_in_richness_bin(
+    sim_config['min_richness'],
+    sim_config['max_richness'],
+    num_drawn=sim_config['num_drawn'],
+    noise_dex=sim_config['sample_noise_dex'])
+
 drawn_nfw_profiles = np.array([
     wlprofile.simulate_nfw(log10mass, concentration, rbins)
     for log10mass, concentration in drawn_mc_pairs
 ])
 
-# Output to an intermediate file to be read by inference example script
-np.save('drawn_nfw_profiles.npy', drawn_nfw_profiles)
-np.save('drawn_mc_pairs.npy', drawn_mc_pairs)
+sim_output_dir = sim_config['sim_output_dir']
 
-np.save('simulated_nfw_profiles.npy', simulated_nfw_profiles)
-np.save('sample_mc_pairs.npy', sample_mc_pairs)
+# Output to an intermediate file to be read by inference example script
+np.save(f'{sim_output_dir}/drawn_nfw_profiles.npy', drawn_nfw_profiles)
+np.save(f'{sim_output_dir}/drawn_mc_pairs.npy', drawn_mc_pairs)
+
+np.save(f'{sim_output_dir}/simulated_nfw_profiles.npy', simulated_nfw_profiles)
+np.save(f'{sim_output_dir}/sample_mc_pairs.npy', sample_mc_pairs)
